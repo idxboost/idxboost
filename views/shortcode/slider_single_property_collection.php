8e5ee00c09abf93021c9342f45109526bbf80898
Antony Rivera <anthony@dgtalliance.com>
2021-05-04 10:40:30 -0500

    v3.7.8

--- views/shortcode/slider_single_property_collection.php
+++ views/shortcode/slider_single_property_collection.php
@@ -1,6 +1,6 @@
 <?php $class_filter = 'js-slider-single-property'; ?>
 
-<section class="flex-block-description mtop-60 ib-filter-slider ib-filter-slider-<?php echo $class_filter; ?>" id="featured-section"
+<section class="flex-block-description mtop-60 ib-filter-slider-property-site ib-filter-slider-<?php echo $class_filter; ?>" id="featured-section"
   data-item="<?php echo $atts['slider_item']; ?>" data-filter="<?php echo $class_filter; ?>"
   auto-play="<?php echo $atts['slider_play']; ?>" speed-slider="<?php echo $atts['slider_speed']; ?>">
 
