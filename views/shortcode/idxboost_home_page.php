7369b33643211150a3fad2bdd589f48b4705ecd3
Maximo Ishi <max@dgtalliance.com>
2023-03-06 18:44:19 -0500

    v4.5.1 released

--- views/shortcode/idxboost_home_page.php
+++ views/shortcode/idxboost_home_page.php
@@ -4,7 +4,7 @@ global $flex_idx_info;
 
 $response = wp_remote_post(IDX_BOOST_SPW_BUILDER_SERVICE . '/api/page-template', array(
         'method' => 'POST',
-        'timeout' => 10,
+        'timeout' => 60,
         'headers' => [
             'Content-Type' => 'application/json',
         ],
